ca5b66ac29ae9d0b62031a0ad16364d883f75752
acboujida <achraf.boujida@etu.uca.fr>
2024-08-23 15:42:06 +0200

    test: breakdown test for when we pass in the path of a directory containing multiple arm project folders

--- cmd/infracost/breakdown_test.go
+++ cmd/infracost/breakdown_test.go
@@ -285,6 +285,13 @@ func TestBreakdownMultiProjectSkipPathsRootLevel(t *testing.T) {
 		}, nil,
 	)
 }
+
+func TestBreakdownArmMultiProject(t *testing.T) {
+	testName := testutil.CalcGoldenFileTestdataDirName()
+	dir := path.Join("./testdata", testName)
+	GoldenFileCommandTest(t, testutil.CalcGoldenFileTestdataDirName(), []string{"breakdown", "--path", dir, "--iac", "arm"}, nil)
+}
+
 func TestBreakdownArmDirectory(t *testing.T) {
 	testName := testutil.CalcGoldenFileTestdataDirName()
 	dir := path.Join("./testdata", testName)
